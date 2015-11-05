1bd03e6e17ddeb84d232631fd6fe6e339669fd09
Thex-PiedDroit <maxime.samyn@etud.infographie-sup.eu>
2015-11-05 09:42:50 +0100

    Changed panels instantiation for Find() for less work on UIManager and better chainwork (if more panels have to be added)

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -13,8 +13,9 @@ public class UIManager
 
 #region Variables (private)
 
-	private Transform pCanvas = null;
+	//private Transform pCanvas = null;
 	private CharacterPanel pCharPanel = null;
+	private InventoryPanel pInvPanel = null;
 
 	private bool m_bUIOpened = false;
 	
@@ -23,32 +24,45 @@ public class UIManager
 
 	public UIManager()
 	{
-		pCanvas = GameObject.FindObjectOfType<Canvas>().gameObject.transform;
-		CharacterPanel pRef = Resources.Load<CharacterPanel>("HUD/CharacPanel");
-		pCharPanel = GameObject.Instantiate(pRef);
-		pCharPanel.gameObject.name = pRef.gameObject.name;
-		pCharPanel.gameObject.transform.SetParent(pCanvas);
-		pCharPanel.transform.localScale = Vector3.one;
-		pCharPanel.transform.localPosition = Vector3.zero;
-		pCharPanel.GetComponent<RawImage>().rectTransform.offsetMin = Vector2.zero;
-		pCharPanel.GetComponent<RawImage>().rectTransform.offsetMax = Vector2.zero;	// Stretch black background to full screen
-		pCharPanel.Initialize();
-		pCharPanel.Refresh();
-		TogglePanel(pCharPanel, false);
+		pCharPanel = GameObject.FindObjectOfType<CharacterPanel>();
+		//pCanvas = GameObject.FindObjectOfType<Canvas>().gameObject.transform;
+		//CharacterPanel pRef = Resources.Load<CharacterPanel>("HUD/CharacPanel");
+		//pCharPanel = GameObject.Instantiate(pRef);
+		//pCharPanel.gameObject.name = pRef.gameObject.name;
+		//pCharPanel.gameObject.transform.SetParent(pCanvas);
+		//pCharPanel.transform.localScale = Vector3.one;
+		//pCharPanel.transform.localPosition = Vector3.zero;
+		//pCharPanel.GetComponent<RawImage>().rectTransform.offsetMin = Vector2.zero;
+		//pCharPanel.GetComponent<RawImage>().rectTransform.offsetMax = Vector2.zero;	// Stretch black background to full screen
+		pInvPanel = GameObject.FindObjectOfType<InventoryPanel>();
+		InitPanel(pCharPanel);
+		InitPanel(pInvPanel);
 	}
 
 	public void Update()
 	{
 		pCharPanel.CatchOpenInput();
+		pInvPanel.CatchOpenInput();
+	}
+
+	void InitPanel(ManaPanel pPanel)
+	{
+		pPanel.Initialize();
+		pPanel.Refresh();
+		TogglePanel(pPanel, false);
 	}
 	
 	public void TogglePanel(ManaPanel pPanel, bool bOpened)
 	{
 		// Disable other panels here
+		pCharPanel.UIElementActive = false;
+		pInvPanel.UIElementActive = false;
+
 		pPanel.UIElementActive = bOpened;	// Currently for tests purposes, will change when more panels to manage
-		
-		bool bStateChanged = (m_bUIOpened != bOpened);	// this too
-		m_bUIOpened = bOpened;
+
+		bool bNewState = pCharPanel.UIElementActive || pInvPanel.UIElementActive;
+		bool bStateChanged = (m_bUIOpened != bNewState);	// this too
+		m_bUIOpened = bNewState;
 
 		if (bStateChanged)
 			OnMenuOpenClose();
